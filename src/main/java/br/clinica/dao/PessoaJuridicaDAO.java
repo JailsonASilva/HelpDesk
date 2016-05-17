79fff936bf47fbf35db8a819cfb02073e2c5c499
Jailson <Jailson@Jailson-PC>
2016-05-17 11:50:45 -0300

    fghg

---
+++ src/main/java/br/clinica/dao/PessoaJuridicaDAO.java
@@ -0,0 +1,7 @@
+package br.clinica.dao;
+
+import br.clinica.domain.PessoaJuridica;
+
+public class PessoaJuridicaDAO extends GenericDAO<PessoaJuridica> {
+
+}
--- src/main/java/br/clinica/domain/Pessoa.java
+++ src/main/java/br/clinica/domain/Pessoa.java
@@ -1,15 +1,36 @@
 package br.clinica.domain;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
-@SuppressWarnings("serial")
 @Entity
-@Table(name = "administrativo_pessoas")
-public class Pessoa extends GenericDomain {
+@Table(name = "administrativo_pessoa")
+public class Pessoa {
+	@Id
+	private Long codigo;
+
 	@Column(length = 100, nullable = false)
 	private String nome;
+
+	@Column(length = 20, nullable = false)
+	private String tipo;
+
+	@Column(nullable = false)
+	@Temporal(TemporalType.DATE)
+	private Date dataCadastro;
+
+	@Column(nullable = false)
+	@Temporal(TemporalType.DATE)
+	private Date dataNascFund;
+
+	@Column(nullable = false)
+	private Boolean inativo;
 
 	public String getNome() {
 		return nome;
@@ -17,6 +38,46 @@ public class Pessoa extends GenericDomain {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Boolean getInativo() {
+		return inativo;
+	}
+
+	public void setInativo(Boolean inativo) {
+		this.inativo = inativo;
+	}
+
+	public Date getDataNascFund() {
+		return dataNascFund;
+	}
+
+	public void setDataNascFund(Date dataNascFund) {
+		this.dataNascFund = dataNascFund;
+	}
+	
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	
+	public Long getCodigo() {
+		return codigo;
 	}
 
 }
